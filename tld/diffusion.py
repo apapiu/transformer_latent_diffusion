b8dd4bf6bbd567e6e8dfb1791882c34cde1800ab
Alexandru Papiu <apapiu@users.noreply.github.com>
2024-01-12 21:20:54 +0000

    clean up diffusion function

--- tld/diffusion.py
+++ tld/diffusion.py
@@ -7,51 +7,43 @@ def diffusion(model,
               device,
               model_dtype=torch.float32,
               n_iter=30,
-              labels=None,
-              num_imgs=64,
+              labels=None, #embeddings to condition on
+              num_imgs=16,
               class_guidance=3,
-              seed=10,
-              scale_factor=8,
-              img_size=16,
+              seed=10, #for reproducibility
+              scale_factor=8, #latent scaling - should be ~ std of latent space
+              img_size=32, #size of latent
               sharp_f = 0.1,
               bright_f = 0.1,
               exponent = 1,
-              seeds = None
+              seeds = None #can input own random latents of size (n_imgs, 4, img_size, img_size)
               ):
-
-    noise_levels = 1 - torch.pow(torch.arange(0.0001, 0.99, 1 / n_iter), exponent)
-    #noise_levels  = 1 - torch.arange(0, 1, 1 / n_iter)
-    noise_levels = noise_levels.tolist()
+    """function to generate images via reverese diffusion - includes decoding the latents."""
+              
+    noise_levels = (1 - torch.pow(torch.arange(0, 1, 1 / n_iter), exponent)).tolist()
 
     if seeds is None:
         torch.manual_seed(seed)
-        seeds = torch.randn(num_imgs,4,img_size,img_size)
-        seeds = seeds.to(device)
+        new_img = torch.randn(num_imgs,4,img_size,img_size, dtype=model_dtype, device=device)
     else:
-        seeds.to(device)
+        new_img = seeds.to(model_dtype, device)
 
-    new_img = seeds
-
-    empty_labels = torch.zeros_like(labels)
-    labels = torch.cat([labels, empty_labels])
-
+    labels = torch.cat([labels, torch.zeros_like(labels)])
     model.eval()
 
     for i in tqdm(range(len(noise_levels) - 1)):
 
         curr_noise, next_noise = noise_levels[i], noise_levels[i + 1]
 
-        noises = torch.full((num_imgs,1), curr_noise)
-        noises = torch.cat([noises, noises])
+        noises = torch.full((2*num_imgs,1), curr_noise)
 
-        new_img = new_img.to(model_dtype)
+        #predict conditional and unconditional latents at the same time:
         x0_pred = model(torch.cat([new_img, new_img]),
                         noises.to(device, model_dtype),
                         labels.to(device, model_dtype)
                         )
 
-        x0_pred_label = x0_pred[:num_imgs]
-        x0_pred_no_label = x0_pred[num_imgs:]
+        x0_pred_label, x0_pred_no_label = x0_pred[:num_imgs], x0_pred[num_imgs:]
 
         # classifier free guidance:
         x0_pred = class_guidance * x0_pred_label + (1 - class_guidance) * x0_pred_no_label
@@ -59,11 +51,9 @@ def diffusion(model,
         # new image at next_noise level is a weighted average of old image and predicted x0:
         new_img = ((curr_noise - next_noise) * x0_pred + next_noise * new_img) / curr_noise
 
-    #TODO: predict with model one more time to get x0
-
+    # TODO: predict with model one more time to get x0
     x0_pred[:, 3, :, :] += sharp_f
     x0_pred[:, 0, :, :] += bright_f
 
     x0_pred_img = vae.decode((x0_pred*scale_factor).half())[0].cpu()
-
     return x0_pred_img, x0_pred
