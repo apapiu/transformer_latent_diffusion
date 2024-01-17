c580e890f3c1eaaddcad7c3ab4525fc26c59861b
Alexandru Papiu <apapiu@users.noreply.github.com>
2024-01-17 18:59:34 +0000

    add dataclass for full config

--- tld/train.py
+++ tld/train.py
@@ -4,9 +4,7 @@ import sys
 import copy
 import numpy as np
 from tqdm import tqdm
-
-from dataclasses import dataclass
-
+from dataclasses import dataclass, asdict
 
 import wandb
 import torchvision.utils as vutils
@@ -25,13 +23,11 @@ sys.path.append('transformer_latent_diffusion')
 from tld.denoiser import Denoiser
 from tld.diffusion import DiffusionGenerator
 
-#!pip install torchview torchmetrics einops wandb diffusers datasets accelerate
-#!git clone https://github.com/apapiu/transformer_latent_diffusion.git
 
 def eval_gen(diffuser, labels):
     class_guidance=4.5
     seed=10
-    out, out_latent = diffuser.generate(labels=torch.repeat_interleave(labels, 8, dim=0),
+    out, _ = diffuser.generate(labels=torch.repeat_interleave(labels, 8, dim=0),
                                         num_imgs=64,
                                         class_guidance=class_guidance,
                                         seed=seed,
@@ -62,17 +58,33 @@ def update_ema(ema_model, model, alpha=0.999):
 
 
 @dataclass
-class TrainConfig:
-    embed_dim: int
-    n_layers: int
-    n_epoch: int
+class ModelConfig:
+    embed_dim: int = 512
+    n_layers: int = 6
+    clip_embed_size: int = 768
+    scaling_factor: int = 8
+    patch_size: int = 2
+    image_size: int = 32 
+    n_channels: int = 4
+    dropout: float = 0
+    mlp_multiplier: int = 4
+    batch_size: int = 128
+    class_guidance: int = 3
+    lr: float = 3e-4
+    n_epoch: int = 100
+    alpha: float = 0.999
+    noise_embed_dims: int = 128
+    diffusion_n_iter: int = 35
+    from_scratch: bool = True
+    run_id: str = None
+    model_name: str = None
 
-def main(config, data_path, val_path):
-    ## see this for more: https://github.com/huggingface/diffusers/blob/main/examples/textual_inversion/textual_inversion.py
-    
+def main(config: ModelConfig, data_path: str, val_path: str):
+    """main train loop to be used with accelerate"""
+
     accelerator = Accelerator(mixed_precision="fp16", log_with="wandb")
 
-    ####DATA LOADING:
+    accelerator.print("Loading Data:")
     latent_train_data = np.load(os.path.join(data_path, 'image_latents.npy'))
     train_label_embeddings = np.load(os.path.join(data_path, 'text_encodings.npy'))
 
@@ -81,74 +93,31 @@ def main(config, data_path, val_path):
     train_label_embeddings = torch.tensor(train_label_embeddings, dtype=torch.float32)
     latent_train_data = torch.tensor(latent_train_data, dtype=torch.float32)
     dataset = TensorDataset(latent_train_data, train_label_embeddings)
+    train_loader = DataLoader(dataset, batch_size=config.batch_size, shuffle=True)
 
     vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix",
                                     torch_dtype=torch.float16)
     
-    if accelerator.is_local_main_process:
+    if accelerator.is_main_process:
         vae = vae.to(accelerator.device)
 
-    ###config: TODO: put all this in a dataclass:
+    accelerator.print("Loading model")
+    model = Denoiser(image_size=config.image_size, noise_embed_dims=config.noise_embed_dims,
+                 patch_size=config.patch_size, embed_dim=config.embed_dim, dropout=config.dropout,
+                 n_layers=config.n_layers)
 
-    from_scratch = True
-    run_id = '3ix3i6qp'
-    model_name = 'curr_state_dict.pth/model.safetensors'
-
-    embed_dim = config.embed_dim
-    n_layers = config.n_layers
-
-    clip_embed_size = 768
-    scaling_factor = 8
-    patch_size = 2
-    image_size = img_size = 32
-    n_channels = 4
-    dropout = 0
-    mlp_multiplier = 4
-
-    batch_size = 128
-    class_guidance = 3
-    lr=3e-4
-
-    alpha = 0.999
-
-    noise_embed_dims = 128
-    diffusion_n_iter = 35
-
-    n_epoch = config.n_epoch
-
-    #end config:
-
-
-    ###model definition
-
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    accelerator.print("loading model")
-
-    model = Denoiser(image_size=image_size, noise_embed_dims=noise_embed_dims,
-                 patch_size=patch_size, embed_dim=embed_dim, dropout=dropout,
-                 n_layers=n_layers)
-
-    if not from_scratch:
-        wandb.restore(model_name, run_path=f"apapiu/cifar_diffusion/runs/{run_id}",
+    if not config.from_scratch:
+        wandb.restore(config.model_name, run_path=f"apapiu/cifar_diffusion/runs/{config.run_id}",
                       replace=True)
-        load_model(model, model_name)
-
-    accelerator.print("model loaded")
+        load_model(model, config.model_name)
 
     if accelerator.is_local_main_process:
         ema_model = copy.deepcopy(model).to(accelerator.device)
         diffuser = DiffusionGenerator(ema_model, vae,  accelerator.device, torch.float32)
 
-    config = {k: v for k, v in locals().items() if k in ['embed_dim', 'n_layers', 'clip_embed_size', 'scaling_factor',
-                                                         'image_size', 'noise_embed_dims', 'dropout',
-                                                         'mlp_multiplier', 'diffusion_n_iter', 'batch_size', 'lr',
-                                                         'patch_size']}
-
-
-    #opt stuff:
     global_step = 0
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
 
     accelerator.print("model prep")
     model, train_loader, optimizer = accelerator.prepare(
@@ -157,7 +126,7 @@ def main(config, data_path, val_path):
 
     accelerator.init_trackers(
     project_name="cifar_diffusion",
-    config=config
+    config=asdict(config)
     )
 
     accelerator.print(count_parameters(model))
@@ -165,11 +134,11 @@ def main(config, data_path, val_path):
 
     ### and train:
 
-    for i in range(1, n_epoch+1):
+    for i in range(1, config.n_epoch+1):
         accelerator.print(f'epoch: {i}')            
 
         for x, y in tqdm(train_loader):
-            x = x/scaling_factor
+            x = x/config.scaling_factor
 
             noise_level = torch.tensor(np.random.beta(1, 2.7, len(x)), device=accelerator.device)
             signal_level = 1 - noise_level
@@ -207,10 +176,10 @@ def main(config, data_path, val_path):
             optimizer.step()
 
             if accelerator.is_local_main_process:
-                update_ema(ema_model, model, alpha=alpha)
+                update_ema(ema_model, model, alpha=config.alpha)
 
             global_step += 1
     accelerator.end_training()
             
-# args = (vae, "fp16")
-# notebook_launcher(training_loop, num_processes=1)
+# args = (config, data_path, val_path)
+# notebook_launcher(training_loop)
