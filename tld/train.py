b503928f524016c83271727b6fcf3080da34ac2a
Alexandru Papiu <apapiu@users.noreply.github.com>
2024-03-04 23:03:41 +0000

    format with ruff

--- tld/train.py
+++ tld/train.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python3
 
 import copy
+from dataclasses import asdict, dataclass
+
 import numpy as np
-from tqdm import tqdm
-from dataclasses import dataclass, asdict
-
-import wandb
-import torchvision.utils as vutils
-
-from accelerate import Accelerator
+import torch
 import torchvision
-
-import torch 
+import torchvision.utils as vutils
+import wandb
+from accelerate import Accelerator
+from diffusers import AutoencoderKL
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
-
-from diffusers import AutoencoderKL
+from tqdm import tqdm
 
 from tld.denoiser import Denoiser
 from tld.diffusion import DiffusionGenerator
 
 
 def eval_gen(diffuser, labels):
-    class_guidance=4.5
-    seed=10
-    out, _ = diffuser.generate(labels=torch.repeat_interleave(labels, 8, dim=0),
-                                        num_imgs=64,
-                                        class_guidance=class_guidance,
-                                        seed=seed,
-                                        n_iter=40,
-                                        exponent=1,
-                                        sharp_f=0.1,
-                                        )
+    class_guidance = 4.5
+    seed = 10
+    out, _ = diffuser.generate(
+        labels=torch.repeat_interleave(labels, 8, dim=0),
+        num_imgs=64,
+        class_guidance=class_guidance,
+        seed=seed,
+        n_iter=40,
+        exponent=1,
+        sharp_f=0.1,
+    )
 
-    out = to_pil((vutils.make_grid((out+1)/2, nrow=8, padding=4)).float().clip(0, 1))
-    out.save(f'emb_val_cfg:{class_guidance}_seed:{seed}.png')
+    out = to_pil((vutils.make_grid((out + 1) / 2, nrow=8, padding=4)).float().clip(0, 1))
+    out.save(f"emb_val_cfg:{class_guidance}_seed:{seed}.png")
 
     return out
 
@@ -42,16 +40,19 @@ def eval_gen(diffuser, labels):
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+
 def count_parameters_per_layer(model):
     for name, param in model.named_parameters():
         print(f"{name}: {param.numel()} parameters")
 
+
 to_pil = torchvision.transforms.ToPILImage()
+
 
 def update_ema(ema_model, model, alpha=0.999):
     with torch.no_grad():
         for ema_param, model_param in zip(ema_model.parameters(), model.parameters()):
-            ema_param.data.mul_(alpha).add_(model_param.data, alpha=1-alpha)
+            ema_param.data.mul_(alpha).add_(model_param.data, alpha=1 - alpha)
 
 
 @dataclass
@@ -61,7 +62,7 @@ class ModelConfig:
     clip_embed_size: int = 768
     scaling_factor: int = 8
     patch_size: int = 2
-    image_size: int = 32 
+    image_size: int = 32
     n_channels: int = 4
     dropout: float = 0
     mlp_multiplier: int = 4
@@ -79,11 +80,13 @@ class ModelConfig:
     beta_b: float = 0.75
     save_and_eval_every_iters: int = 1000
 
+
 @dataclass
 class DataConfig:
-    latent_path: str #path to a numpy file containing latents
+    latent_path: str  # path to a numpy file containing latents
     text_emb_path: str
     val_path: str
+
 
 def main(config: ModelConfig, dataconfig: DataConfig):
     """main train loop to be used with accelerate"""
@@ -97,17 +100,20 @@ def main(config: ModelConfig, dataconfig: DataConfig):
     dataset = TensorDataset(latent_train_data, train_label_embeddings)
     train_loader = DataLoader(dataset, batch_size=config.batch_size, shuffle=True)
 
-    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix",
-                                    torch_dtype=torch.float16)
-    
+    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+
     if accelerator.is_main_process:
         vae = vae.to(accelerator.device)
-   
-    model = Denoiser(image_size=config.image_size, noise_embed_dims=config.noise_embed_dims,
-                 patch_size=config.patch_size, embed_dim=config.embed_dim, dropout=config.dropout,
-                 n_layers=config.n_layers)
-    
-    
+
+    model = Denoiser(
+        image_size=config.image_size,
+        noise_embed_dims=config.noise_embed_dims,
+        patch_size=config.patch_size,
+        embed_dim=config.embed_dim,
+        dropout=config.dropout,
+        n_layers=config.n_layers,
+    )
+
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
 
@@ -116,45 +122,42 @@ def main(config: ModelConfig, dataconfig: DataConfig):
 
     if not config.from_scratch:
         accelerator.print("Loading Model:")
-        wandb.restore(config.model_name, run_path=f"apapiu/cifar_diffusion/runs/{config.run_id}",
-                      replace=True)
+        wandb.restore(
+            config.model_name, run_path=f"apapiu/cifar_diffusion/runs/{config.run_id}", replace=True
+        )
         full_state_dict = torch.load(config.model_name)
-        model.load_state_dict(full_state_dict['model_ema'])
-        optimizer.load_state_dict(full_state_dict['opt_state'])
-        global_step = full_state_dict['global_step']
+        model.load_state_dict(full_state_dict["model_ema"])
+        optimizer.load_state_dict(full_state_dict["opt_state"])
+        global_step = full_state_dict["global_step"]
     else:
         global_step = 0
 
     if accelerator.is_local_main_process:
         ema_model = copy.deepcopy(model).to(accelerator.device)
-        diffuser = DiffusionGenerator(ema_model, vae,  accelerator.device, torch.float32)
+        diffuser = DiffusionGenerator(ema_model, vae, accelerator.device, torch.float32)
 
     accelerator.print("model prep")
-    model, train_loader, optimizer = accelerator.prepare(
-        model, train_loader, optimizer
-    )
+    model, train_loader, optimizer = accelerator.prepare(model, train_loader, optimizer)
 
-    accelerator.init_trackers(
-    project_name="cifar_diffusion",
-    config=asdict(config)
-    )
+    accelerator.init_trackers(project_name="cifar_diffusion", config=asdict(config))
 
     accelerator.print(count_parameters(model))
     accelerator.print(count_parameters_per_layer(model))
 
     ### Train:
-    for i in range(1, config.n_epoch+1):
-        accelerator.print(f'epoch: {i}')            
+    for i in range(1, config.n_epoch + 1):
+        accelerator.print(f"epoch: {i}")
 
         for x, y in tqdm(train_loader):
-            x = x/config.scaling_factor
+            x = x / config.scaling_factor
 
-            noise_level = torch.tensor(np.random.beta(config.beta_a, config.beta_b, len(x)),
-                                        device=accelerator.device)
+            noise_level = torch.tensor(
+                np.random.beta(config.beta_a, config.beta_b, len(x)), device=accelerator.device
+            )
             signal_level = 1 - noise_level
             noise = torch.randn_like(x)
 
-            x_noisy = noise_level.view(-1,1,1,1)*noise + signal_level.view(-1,1,1,1)*x
+            x_noisy = noise_level.view(-1, 1, 1, 1) * noise + signal_level.view(-1, 1, 1, 1) * x
 
             x_noisy = x_noisy.float()
             noise_level = noise_level.float()
@@ -162,21 +165,22 @@ def main(config: ModelConfig, dataconfig: DataConfig):
 
             prob = 0.15
             mask = torch.rand(y.size(0), device=accelerator.device) < prob
-            label[mask] = 0 # OR replacement_vector
+            label[mask] = 0  # OR replacement_vector
 
             if global_step % config.save_and_eval_every_iters == 0:
                 accelerator.wait_for_everyone()
                 if accelerator.is_main_process:
                     ##eval and saving:
                     out = eval_gen(diffuser=diffuser, labels=emb_val)
-                    out.save('img.jpg')
+                    out.save("img.jpg")
                     accelerator.log({f"step: {global_step}": wandb.Image("img.jpg")})
-                    
+
                     opt_unwrapped = accelerator.unwrap_model(optimizer)
-                    full_state_dict = {'model_ema':ema_model.state_dict(),
-                                    'opt_state':opt_unwrapped.state_dict(),
-                                    'global_step':global_step
-                                    }
+                    full_state_dict = {
+                        "model_ema": ema_model.state_dict(),
+                        "opt_state": opt_unwrapped.state_dict(),
+                        "global_step": global_step,
+                    }
                     accelerator.save(full_state_dict, config.model_name)
                     wandb.save(config.model_name)
 
@@ -186,9 +190,9 @@ def main(config: ModelConfig, dataconfig: DataConfig):
                 ###train loop:
                 optimizer.zero_grad()
 
-                pred = model(x_noisy, noise_level.view(-1,1), label)
+                pred = model(x_noisy, noise_level.view(-1, 1), label)
                 loss = loss_fn(pred, x)
-                accelerator.log({"train_loss":loss.item()}, step=global_step)
+                accelerator.log({"train_loss": loss.item()}, step=global_step)
                 accelerator.backward(loss)
                 optimizer.step()
 
@@ -197,6 +201,7 @@ def main(config: ModelConfig, dataconfig: DataConfig):
 
             global_step += 1
     accelerator.end_training()
-            
+
+
 # args = (config, data_path, val_path)
 # notebook_launcher(training_loop)
--- tld/transformer_blocks.py
+++ tld/transformer_blocks.py
@@ -3,20 +3,23 @@ import torch.nn as nn
 import numpy as np
 from einops import rearrange
 
+
 class SinusoidalEmbedding(nn.Module):
     def __init__(self, emb_min_freq=1.0, emb_max_freq=1000.0, embedding_dims=32):
         super(SinusoidalEmbedding, self).__init__()
 
         frequencies = torch.exp(
-            torch.linspace(np.log(emb_min_freq), np.log(emb_max_freq),
-                embedding_dims // 2))
+            torch.linspace(np.log(emb_min_freq), np.log(emb_max_freq), embedding_dims // 2)
+        )
 
-        self.register_buffer('angular_speeds', 2.0 * torch.pi * frequencies)
+        self.register_buffer("angular_speeds", 2.0 * torch.pi * frequencies)
 
     def forward(self, x):
-        embeddings = torch.cat([torch.sin(self.angular_speeds * x),
-                                torch.cos(self.angular_speeds * x)], dim=-1)
+        embeddings = torch.cat(
+            [torch.sin(self.angular_speeds * x), torch.cos(self.angular_speeds * x)], dim=-1
+        )
         return embeddings
+
 
 class MHAttention(nn.Module):
     def __init__(self, is_causal=False, dropout_level=0, n_heads=4):
@@ -26,42 +29,48 @@ class MHAttention(nn.Module):
         self.n_heads = n_heads
 
     def forward(self, q, k, v, attn_mask=None):
-
         assert q.size(-1) == k.size(-1)
         assert k.size(-2) == v.size(-2)
 
-        q, k, v = [rearrange(x, 'bs n (h d) -> bs h n d', h=self.n_heads) for x in [q,k,v]]
+        q, k, v = [rearrange(x, "bs n (h d) -> bs h n d", h=self.n_heads) for x in [q, k, v]]
 
-        out = nn.functional.scaled_dot_product_attention(q, k, v,
-                                                          attn_mask=attn_mask,
-                                                          is_causal=self.is_causal,
-                                                          dropout_p=self.dropout_level if self.training else 0)
-        
-        out = rearrange(out, 'bs h n d -> bs n (h d)', h=self.n_heads)
+        out = nn.functional.scaled_dot_product_attention(
+            q,
+            k,
+            v,
+            attn_mask=attn_mask,
+            is_causal=self.is_causal,
+            dropout_p=self.dropout_level if self.training else 0,
+        )
+
+        out = rearrange(out, "bs h n d -> bs n (h d)", h=self.n_heads)
 
         return out
+
 
 class SelfAttention(nn.Module):
     def __init__(self, embed_dim, is_causal=False, dropout_level=0, n_heads=4):
         super().__init__()
-        self.qkv_linear = nn.Linear(embed_dim, 3*embed_dim, bias=False)
+        self.qkv_linear = nn.Linear(embed_dim, 3 * embed_dim, bias=False)
         self.mha = MHAttention(is_causal, dropout_level, n_heads)
 
     def forward(self, x):
         q, k, v = self.qkv_linear(x).chunk(3, dim=2)
         return self.mha(q, k, v)
 
+
 class CrossAttention(nn.Module):
     def __init__(self, embed_dim, is_causal=False, dropout_level=0, n_heads=4):
         super().__init__()
-        self.kv_linear = nn.Linear(embed_dim, 2*embed_dim, bias=False)
+        self.kv_linear = nn.Linear(embed_dim, 2 * embed_dim, bias=False)
         self.q_linear = nn.Linear(embed_dim, embed_dim, bias=False)
         self.mha = MHAttention(is_causal, dropout_level, n_heads)
 
     def forward(self, x, y):
         q = self.q_linear(x)
         k, v = self.kv_linear(y).chunk(2, dim=2)
-        return self.mha(q,k,v)
+        return self.mha(q, k, v)
+
 
 class MLP(nn.Module):
     def __init__(self, embed_dim, mlp_multiplier, dropout_level):
@@ -70,38 +79,47 @@ class MLP(nn.Module):
             nn.Linear(embed_dim, mlp_multiplier * embed_dim),
             nn.GELU(),
             nn.Linear(mlp_multiplier * embed_dim, embed_dim),
-            nn.Dropout(dropout_level)
+            nn.Dropout(dropout_level),
         )
 
     def forward(self, x):
         return self.mlp(x)
+
 
 class MLPSepConv(nn.Module):
     def __init__(self, embed_dim, mlp_multiplier, dropout_level):
         """see: https://github.com/ofsoundof/LocalViT"""
         super().__init__()
         self.mlp = nn.Sequential(
-            #this Conv with kernel size 1 is equivalent to the Linear layer in a "regular" transformer MLP
-            nn.Conv2d(embed_dim, mlp_multiplier*embed_dim, kernel_size=1, padding='same'),
-            nn.Conv2d(mlp_multiplier*embed_dim, mlp_multiplier*embed_dim, kernel_size=3,
-                      padding='same', groups=mlp_multiplier*embed_dim), #<- depthwise conv
+            # this Conv with kernel size 1 is equivalent to the Linear layer in a "regular" transformer MLP
+            nn.Conv2d(embed_dim, mlp_multiplier * embed_dim, kernel_size=1, padding="same"),
+            nn.Conv2d(
+                mlp_multiplier * embed_dim,
+                mlp_multiplier * embed_dim,
+                kernel_size=3,
+                padding="same",
+                groups=mlp_multiplier * embed_dim,
+            ),  # <- depthwise conv
             nn.GELU(),
-            nn.Conv2d(mlp_multiplier*embed_dim, embed_dim, kernel_size=1, padding='same'),
-            nn.Dropout(dropout_level)
-            )
+            nn.Conv2d(mlp_multiplier * embed_dim, embed_dim, kernel_size=1, padding="same"),
+            nn.Dropout(dropout_level),
+        )
 
     def forward(self, x):
-        w = h = int(np.sqrt(x.size(1))) #only square images for now
-        x = rearrange(x, 'bs (h w) d -> bs d h w', h=h, w=w)
+        w = h = int(np.sqrt(x.size(1)))  # only square images for now
+        x = rearrange(x, "bs (h w) d -> bs d h w", h=h, w=w)
         x = self.mlp(x)
-        x = rearrange(x, 'bs d h w -> bs (h w) d')
+        x = rearrange(x, "bs d h w -> bs (h w) d")
         return x
+
 
 class DecoderBlock(nn.Module):
     def __init__(self, embed_dim, is_causal, mlp_multiplier, dropout_level, mlp_class=MLP):
         super().__init__()
-        self.self_attention = SelfAttention(embed_dim, is_causal, dropout_level, n_heads=embed_dim//64)
-        self.cross_attention = CrossAttention(embed_dim, is_causal=False, dropout_level=0, n_heads=embed_dim//64)
+        self.self_attention = SelfAttention(embed_dim, is_causal, dropout_level, n_heads=embed_dim // 64)
+        self.cross_attention = CrossAttention(
+            embed_dim, is_causal=False, dropout_level=0, n_heads=embed_dim // 64
+        )
         self.mlp = mlp_class(embed_dim, mlp_multiplier, dropout_level)
         self.norm1 = nn.LayerNorm(embed_dim)
         self.norm2 = nn.LayerNorm(embed_dim)
